56bb3bda952160228c5ec549bb62a0db00d104af
satyajit-patel <satyajitpatel770001@gmail.com>
2025-04-24 11:02:48 +0530

    Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex justify-center items-center bg-fuchsia-300">
       <div>
         <h1>Hi Next js</h1>
+        <Link href="/team">
+          click me to go to team
+        </Link>
       </div>
     </div>
   );
